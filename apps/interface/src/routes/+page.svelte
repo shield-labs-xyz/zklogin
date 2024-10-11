c9b3dd9039e895960485ca85c454382930337914
Oleh Misarosh <oleh.blockchain@gmail.com>
2024-10-11 22:25:25 +0200

    add owner

--- apps/interface/src/routes/+page.svelte
+++ apps/interface/src/routes/+page.svelte
@@ -61,4 +61,47 @@
       </Ui.Form>
     </Ui.Card.Content>
   </Ui.Card>
+
+  <Ui.Card>
+    <Ui.Card.Header>
+      <Ui.Card.Title>Add owner</Ui.Card.Title>
+    </Ui.Card.Header>
+    <Ui.Card.Content>
+      <Ui.Form
+        schema={z.object({
+          owner: zAddress(),
+        })}
+        onsubmit={async (data) => {
+          utils.assertConnected(lib.web3modal.account);
+          const signer = await lib.web3modal.account.getSigner();
+          console.log(await signer.getAddress());
+          const abi = ["function addOwnerAddress(address owner)"];
+          const contract = new ethers.Contract(
+            await signer.getAddress(),
+            abi,
+            signer,
+          );
+          // unfortunately, this does not work. coinbase.com shows "something went wrong". Probably a guard against scammers
+          const tx = await contract.addOwnerAddress!(data.owner);
+        }}
+      >
+        {#snippet children(form, formData)}
+          <Ui.Form.Field {form} name="owner">
+            <Ui.Form.Control let:attrs>
+              <Ui.Form.Label>Address</Ui.Form.Label>
+              <Ui.Input {...attrs} bind:value={formData.owner} />
+            </Ui.Form.Control>
+            <Ui.Form.Description></Ui.Form.Description>
+            <Ui.Form.FieldErrors />
+          </Ui.Form.Field>
+
+          <ConnectWalletOr class="w-full">
+            <Ui.Form.SubmitButton variant="default">
+              Add owner
+            </Ui.Form.SubmitButton>
+          </ConnectWalletOr>
+        {/snippet}
+      </Ui.Form>
+    </Ui.Card.Content>
+  </Ui.Card>
 </Ui.GapContainer>
