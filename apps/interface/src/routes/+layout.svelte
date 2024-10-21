ef842cbf6807a1392a7bd3e59971bd28b53edbb4
Oleh Misarosh <olehmisar@gmail.com>
2024-10-22 01:29:31 +0200

    feat: add analytics

--- apps/interface/src/routes/+layout.svelte
+++ apps/interface/src/routes/+layout.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
   import "../app.css";
 
+  import { dev } from "$app/environment";
   import { lib } from "$lib";
   import { Ui } from "@repo/ui";
   import { QueryClientProvider } from "@tanstack/svelte-query";
+  import { inject } from "@vercel/analytics";
   import Header from "./Header.svelte";
 
   let { children } = $props();
+
+  inject({ mode: dev ? "development" : "production" });
 </script>
 
 <QueryClientProvider client={lib.queries.queryClient}>
