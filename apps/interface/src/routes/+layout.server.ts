37f79a365c0a8dfcdbe0600ed89113573bd1134f
Oleh Misarosh <olehmisar@gmail.com>
2024-10-24 19:15:15 +0200

    fix: enable multithreaded proving

--- apps/interface/src/routes/+layout.server.ts
+++ apps/interface/src/routes/+layout.server.ts
@@ -1,6 +1,12 @@
 import type { Session } from "@auth/sveltekit";
 
 export async function load(event) {
+  // set these headers to enable multithreaded proving
+  event.setHeaders({
+    "Cross-Origin-Embedder-Policy": "require-corp",
+    "Cross-Origin-Opener-Policy": "same-origin",
+  });
+
   const session = (await event.locals.auth()) as
     | (Session & { id_token?: string })
     | null;
